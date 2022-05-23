3bed6c72924cb7ed360219ddde2037098d8aa7de
Continuara <bizneslm2@gmail.com>
2022-05-23 10:07:13 -0300

    coder js

---
+++ index.js
@@ -0,0 +1,49 @@
+/* //actividad 1
+let usuario = prompt("ingrese un nombre: mariano")
+let nombre = "mariano"
+
+if (usuario === nombre) {
+    alert("FUI YO")
+}
+
+else {
+    alert("NO FUI YO")
+} */
+
+//actividad 2
+
+//actividad 3
+/* let entrada3 = prompt("Seleccione un jugador del 1 al 4");
+
+if (entrada3 == 1) {
+    alert("ELEGISTE A HOMERO");
+}else if (entrada3 == 2) {
+    alert("ELEGISTE A MARGE");
+}else if (entrada3 == 3) {
+    alert("ELEGISTE A BART");
+}else if (entrada3 == 4) {
+    alert("ELEGISTE A LISA");
+}
+else {
+    alert("ERROR"); 
+} */
+
+//actividad 4
+/*  let usuario = Number(prompt("ingrese un numero entre 0 y 3000"))
+let numero = 1000
+let numero1 = 1001
+let numero2= 3000
+
+if (usuario <= numero ) {
+    alert("presupuesto bajo")
+}
+else if (usuario >= numero1; usuario < numero2;) {
+    alert("presupuesto medio")
+}
+else if (usuario >= numero2) {
+    alert("presupuesto alto") 
+} */
+
+//actividad 5
+
+
